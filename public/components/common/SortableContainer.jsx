a9e5438c2f58a17a4af7c7f19594aa9cdec6ce4f
Jonathon Kelly <jonathon.kelly@insidersbyte.com>
2016-05-09 20:42:16 +0100

    update package, activate alt debug

--- public/components/common/SortableContainer.jsx
+++ public/components/common/SortableContainer.jsx
@@ -2,16 +2,17 @@ import React from 'react';
 import { DragDropContext as dragDropContext } from 'react-dnd';
 import HTML5Backend from 'react-dnd-html5-backend';
 
-function SortableContainer(props) {
-    return (
-        <div>
-            {props.children}
-        </div>
-    );
+@dragDropContext(HTML5Backend)
+export default class SortableContainer extends React.Component { // eslint-disable-line react/prefer-stateless-function
+    static propTypes = {
+        children: React.PropTypes.element.isRequired,
+    };
+
+    render() {
+        return (
+            <div>
+                {this.props.children}
+            </div>
+        );
+    }
 }
-
-SortableContainer.propTypes = {
-    children: React.PropTypes.element.isRequired,
-};
-
-export default dragDropContext(HTML5Backend)(SortableContainer);
