81e2d64459226c60e49732d10d28adb051fcaf15
Jonathon Kelly <jonathon@insidersbyte.com>
2016-10-01 22:26:35 +0100

    move profile over to redux

--- public/actions/users.js
+++ public/actions/users.js
@@ -7,15 +7,6 @@ import { HTTP_METHODS } from '../constants/api';
 export const openUserModal = createAction(TYPES.OPEN_USER_MODAL);
 export const closeUserModal = createAction(TYPES.CLOSE_USER_MODAL);
 
-function onCreateUserSuccess(dispatch) {
-    dispatch(success({ message: 'User created successfully' }));
-    dispatch(closeUserModal());
-}
-
-function onDeleteUserSuccess(dispatch) {
-    dispatch(success({ message: 'User deleted successfully' }));
-}
-
 export function loadUsers() {
     return {
         [CALL_API]: {
@@ -34,7 +25,10 @@ export function createUser(user) {
             endpoint: 'user',
             method: HTTP_METHODS.POST,
             authenticated: true,
-            onSuccess: onCreateUserSuccess,
+            onSuccess: (dispatch) => {
+                dispatch(success({ message: 'User created successfully' }));
+                dispatch(closeUserModal());
+            },
             types: [TYPES.CREATE_USER_REQUEST, TYPES.CREATE_USER_SUCCESS, TYPES.CREATE_USER_ERROR],
         },
     };
@@ -47,8 +41,25 @@ export function deleteUser(user) {
             endpoint: `user/${user.id}`,
             method: HTTP_METHODS.DELETE,
             authenticated: true,
-            onSuccess: onDeleteUserSuccess,
+            onSuccess: (dispatch) => {
+                dispatch(success({ message: 'User deleted successfully' }));
+            },
             types: [TYPES.DELETE_USER_REQUEST, TYPES.DELETE_USER_SUCCESS, TYPES.DELETE_USER_ERROR],
+        },
+    };
+}
+
+export function changePassword(user) {
+    return {
+        [CALL_API]: {
+            data: user,
+            endpoint: 'user/password',
+            method: HTTP_METHODS.PUT,
+            authenticated: true,
+            onSuccess: (dispatch) => {
+                dispatch(success({ message: 'Password Changed' }));
+            },
+            types: [TYPES.CHANGE_PASSWORD_REQUEST, TYPES.CHANGE_PASSWORD_SUCCESS, TYPES.CHANGE_PASSWORD_ERROR],
         },
     };
 }
