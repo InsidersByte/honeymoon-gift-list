6e19a911d168f5526dc776489a962fd2d386374f
Jonathon Kelly <jonathon@insidersbyte.com>
2016-10-01 16:12:40 +0100

    decode token on login success

--- public/reducers/auth.js
+++ public/reducers/auth.js
@@ -1,4 +1,5 @@
 import { LOGIN_REQUEST, LOGIN_SUCCESS, LOGIN_ERROR, LOGOUT } from '../constants/actionTypes';
+import jwtDecoder from '../utils/jwtDecoder';
 
 const auth = {
     user: {},
@@ -11,14 +12,18 @@ export default function authReducer(state = auth, action) {
         case LOGIN_REQUEST:
             return Object.assign({}, state, { saving: true });
 
-        case LOGIN_SUCCESS:
-            return Object.assign({}, state, { isAuthenticated: true, saving: false });
+        case LOGIN_SUCCESS: {
+            const { token } = action.payload;
+            const user = jwtDecoder(token);
+
+            return Object.assign({}, state, { user, isAuthenticated: true, saving: false });
+        }
 
         case LOGIN_ERROR:
             return Object.assign({}, state, { saving: false });
 
         case LOGOUT:
-            return Object.assign({}, state, { isAuthenticated: false, saving: false });
+            return Object.assign({}, state, { user: {}, isAuthenticated: false, saving: false });
 
         default:
             return state;
