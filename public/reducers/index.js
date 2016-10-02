0ad7fff0fbb5b6dbfa7eef443b15265183704269
Jonathon Kelly <jonathon@insidersbyte.com>
2016-10-02 18:47:30 +0100

    alter sign up

--- public/reducers/index.js
+++ public/reducers/index.js
@@ -6,11 +6,13 @@ import auth from './auth';
 import users from './users';
 import notifications from './notifications';
 import weddingProfile from './weddingProfile';
+import signUp from './signUp';
 
 export default combineReducers({
     auth,
     users,
     notifications,
     weddingProfile,
+    signUp,
     routing,
 });
