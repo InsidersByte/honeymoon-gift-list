8305b845ab367a85f4c0771c31494d753355e365
Jonathon Kelly <jonathon.kelly@insidersbyte.com>
2017-03-02 15:33:24 +0000

    test Loader default props

--- public/components/Loader/index.test.jsx
+++ public/components/Loader/index.test.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import ReactLoader from 'react-loader';
 import Loader from './';
 
 describe('Loader', () => {
@@ -7,9 +8,20 @@ describe('Loader', () => {
         const wrapper = shallow(
             <Loader loading className="loader">
                 <h1>Hello World</h1>
-            </Loader>
+            </Loader>,
         );
 
         expect(wrapper).toMatchSnapshot();
+    });
+
+    it('should default to loading true', () => {
+        const wrapper = shallow(
+            <Loader className="loader">
+                <h1>Hello World</h1>
+            </Loader>,
+        );
+
+        expect(wrapper.find(ReactLoader).length).toBe(1);
+        expect(wrapper.find(ReactLoader).prop('loaded')).toBe(false);
     });
 });
